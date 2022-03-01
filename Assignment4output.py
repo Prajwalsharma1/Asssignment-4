eecbdf14392ddf479a828c22b6838930fe8d48d1
Prajwalsharma1 <97334755+Prajwalsharma1@users.noreply.github.com>
2022-03-01 10:26:57 -0800

    Add files via upload

---
+++ Assignment4output.py
@@ -0,0 +1,64 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: C:/Users/prajw/Desktop/Assignment4input.py =============
+ Question 1
+enter the number of disks>>4
+ Move disk 1 from rod A to rod C
+ move disk 2 from rod A to rod B
+ Move disk 1 from rod C to rod B
+ move disk 3 from rod A to rod C
+ Move disk 1 from rod B to rod A
+ move disk 2 from rod B to rod C
+ Move disk 1 from rod A to rod C
+ move disk 4 from rod A to rod B
+ Move disk 1 from rod C to rod B
+ move disk 2 from rod C to rod A
+ Move disk 1 from rod B to rod A
+ move disk 3 from rod C to rod B
+ Move disk 1 from rod A to rod C
+ move disk 2 from rod A to rod B
+ Move disk 1 from rod C to rod B
+ -----Done----
+Question 2
+enter the Length of pascal triangle >> 6
+By loop Method
+         1
+    1  5  10  10  5  1
+Enter the length of pascal triangle>>7
+By Recursion
+1 
+1 1 
+1 2 1 
+1 3 3 1 
+1 4 6 4 1 
+1 5 10 10 5 1 
+1 6 15 20 15 6 1 
+Question 3
+Enter the 1st number>>6
+Enter the 2nd number >>7
+Part(A)
+Quotient  is callable >> False
+Remainder is callable >> False
+Part(B)
+quotient is zero
+Part(c)
+The values greater than 4 is contained in this list  [10, 5, 11, 6, 12]
+Part(D)
+our set is >> {5, 6, 10, 11, 12}
+Part (E)
+Immutable set is >> frozenset({5, 6, 10, 11, 12})
+Part(F)
+The required hash value of maximum valued element of set is  12
+ Question 4 
+Name is ankit sharma and Roll no.  is 21103021
+Your object is destroyed
+Question (5) 
+Part(a)
+The new salary of Mehak is 70000
+The employee record of Viren is deleted
+Question (6)
+George please enter a word>>pat
+Enter the word you want to check >>tap
+Is the word is make any sense (y/n)>>p
+your friendship is fake
